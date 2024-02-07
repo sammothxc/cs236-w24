459c0088d828782e72f28948454dca11442d3288
sammothxc <sdwarrior8@gmail.com>
2024-02-06 19:41:15 -0700

    update

--- labs/lab2/Parser.h
+++ labs/lab2/Parser.h
@@ -2,8 +2,20 @@
 #include Token.h
 
 class Parser {
- private:
-  vector<Token> tokens;
- public:
-  Parser(const vector<Token>& tokens) : tokens(tokens) { }
+    private:
+        vector<Token> tokens;
+    public:
+        Parser(const vector<Token>& tokens) : tokens(tokens) { }
+
+        TokenType tokenType() const {
+            return tokens.at(0).getType();
+        }
+
+        void advanceToken() {
+        tokens.erase(tokens.begin());
+        }
+        
+        void throwError() {
+            cout << "error" << endl;
+        }
 };
