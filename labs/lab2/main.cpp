006937a7a2c5ffc9954d1c5b505e3b4ac497f251
sammothxc <sdwarrior8@gmail.com>
2024-02-06 20:22:55 -0700

    update

--- labs/lab2/main.cpp
+++ labs/lab2/main.cpp
@@ -7,14 +7,15 @@ using namespace std;
 int main() {
 
   vector<Token> tokens = {
-    Token(COMMA,",",2),
-    //Token(ID,"Ted",2),
+    Token(ID,"Ned",2),
+    //Token(LEFT_PAREN,"(",2),
+    Token(ID,"Ted",2),
     Token(COMMA,",",2),
     Token(ID,"Zed",2),
     Token(RIGHT_PAREN,")",2),
   };
 
   Parser p = Parser(tokens);
-  p.idList();
+  p.scheme();
 
 }
