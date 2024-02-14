4ba3f699d3adac9bc65b7bb2fa49474d175f5c60
sammothxc <sdwarrior8@gmail.com>
2024-02-13 23:29:02 -0700

    update

--- projects/project2/mainp.cpp
+++ projects/project2/mainp.cpp
@@ -1,346 +1,326 @@
 #include <iostream>
 #include <vector>
-#include <string>
 #include <set>
-
-using namespace std;
+#include <stdexcept>
 
 // Forward declaration of classes
-class Predicate;
 class Parameter;
+class Predicate;
+class Rule;
+class DatalogProgram;
 
-// Class to represent a rule in the Datalog program
-class Rule {
+// Class representing a Parameter in a Predicate
+class Parameter {
 public:
-    Predicate* headPredicate;
-    vector<Predicate*> bodyPredicates;
+    std::string value;
+    bool isConstant;
 
-    Rule(Predicate* headPred) : headPredicate(headPred) {}
-    ~Rule() {
-        delete headPredicate;
-        for (auto& pred : bodyPredicates)
-            delete pred;
-    }
-    string toString() const {
-        string result = headPredicate->toString() + " :- ";
-        for (size_t i = 0; i < bodyPredicates.size(); ++i) {
-            if (i != 0)
-                result += ",";
-            result += bodyPredicates[i]->toString();
-        }
-        result += ".";
-        return result;
+    Parameter(const std::string& val, bool constant) : value(val), isConstant(constant) {}
+
+    std::string toString() const {
+        return value;
     }
 };
 
-// Class to represent a predicate in the Datalog program
+// Class representing a Predicate in a Datalog program
 class Predicate {
 public:
-    string name;
-    vector<Parameter*> parameters;
+    std::string name;
+    std::vector<Parameter> parameters;
 
-    Predicate(const string& n) : name(n) {}
-    ~Predicate() {
-        for (auto& param : parameters)
-            delete param;
-    }
-    string toString() const {
-        string result = name + "(";
+    Predicate(const std::string& n) : name(n) {}
+
+    std::string toString() const {
+        std::string result = name + "(";
         for (size_t i = 0; i < parameters.size(); ++i) {
-            if (i != 0)
+            result += parameters[i].toString();
+            if (i != parameters.size() - 1)
                 result += ",";
-            result += parameters[i]->toString();
         }
         result += ")";
         return result;
     }
 };
 
-// Class to represent a parameter in the Datalog program
-class Parameter {
+// Class representing a Rule in a Datalog program
+class Rule {
 public:
-    string value;
-    bool isString;
+    Predicate headPredicate;
+    std::vector<Predicate> bodyPredicates;
 
-    Parameter(const string& v, bool isStr) : value(v), isString(isStr) {}
-    string toString() const {
-        if (isString)
-            return "'" + value + "'";
-        else
-            return value;
+    Rule(const Predicate& head) : headPredicate(head) {}
+
+    std::string toString() const {
+        std::string result = headPredicate.toString() + " :- ";
+        for (size_t i = 0; i < bodyPredicates.size(); ++i) {
+            result += bodyPredicates[i].toString();
+            if (i != bodyPredicates.size() - 1)
+                result += ",";
+        }
+        return result;
     }
 };
 
-// Class to represent the entire Datalog program
+// Class representing the entire Datalog program
 class DatalogProgram {
 public:
-    vector<Predicate*> schemes;
-    vector<Predicate*> facts;
-    vector<Rule*> rules;
-    vector<Predicate*> queries;
-    set<string> domain;
+    std::vector<Predicate> schemes;
+    std::vector<Predicate> facts;
+    std::vector<Rule> rules;
+    std::vector<Predicate> queries;
+    std::set<std::string> domain;
 
-    ~DatalogProgram() {
-        for (auto& scheme : schemes)
-            delete scheme;
-        for (auto& fact : facts)
-            delete fact;
-        for (auto& rule : rules)
-            delete rule;
-        for (auto& query : queries)
-            delete query;
+    std::string toString() const {
+        std::string result = "Schemes(" + std::to_string(schemes.size()) + "):\n";
+        for (const auto& scheme : schemes) {
+            result += "  " + scheme.toString() + "\n";
+        }
+        result += "Facts(" + std::to_string(facts.size()) + "):\n";
+        for (const auto& fact : facts) {
+            result += "  " + fact.toString() + "\n";
+        }
+        result += "Rules(" + std::to_string(rules.size()) + "):\n";
+        for (const auto& rule : rules) {
+            result += "  " + rule.toString() + "\n";
+        }
+        result += "Queries(" + std::to_string(queries.size()) + "):\n";
+        for (const auto& query : queries) {
+            result += "  " + query.toString() + "\n";
+        }
+        result += "Domain(" + std::to_string(domain.size()) + "):\n";
+        for (const auto& value : domain) {
+            result += "  " + value + "\n";
+        }
+        return result;
+    }
+};
+
+// Token types
+enum class TokenType { SCHEMES, FACTS, RULES, QUERIES, COLON, COMMA, PERIOD, Q_MARK, LEFT_PAREN, RIGHT_PAREN, ID, STRING, EOF_TYPE };
+
+// Scanner class for tokenizing input
+class Scanner {
+public:
+    Scanner(std::istream& input) : input(input), lineNumber(1) {}
+
+    TokenType getNextToken() {
+        char ch;
+        while (input.get(ch)) {
+            switch (ch) {
+                case ' ':
+                case '\t':
+                case '\r':
+                    continue; // Ignore whitespace
+                case '\n':
+                    ++lineNumber;
+                    continue; // Ignore newline
+                case ':':
+                    return TokenType::COLON;
+                case ',':
+                    return TokenType::COMMA;
+                case '.':
+                    return TokenType::PERIOD;
+                case '?':
+                    return TokenType::Q_MARK;
+                case '(':
+                    return TokenType::LEFT_PAREN;
+                case ')':
+                    return TokenType::RIGHT_PAREN;
+                case '\'':
+                    return readString();
+                default:
+                    if (isalpha(ch)) {
+                        input.putback(ch); // Put back the first character of the identifier
+                        return readIdentifier();
+                    } else {
+                        throw std::runtime_error("Invalid character '" + std::string(1, ch) + "' on line " + std::to_string(lineNumber));
+                    }
+            }
+        }
+        return TokenType::EOF_TYPE;
     }
 
-    void addToDomain(const string& value) {
-        domain.insert(value);
+    std::string getTokenValue() const {
+        return tokenValue;
     }
 
-    void print() const {
-        cout << "Success!" << endl;
-        printList("Schemes", schemes);
-        printList("Facts", facts);
-        printList("Rules", rules);
-        printList("Queries", queries);
-        cout << "Domain(" << domain.size() << "):" << endl;
-        for (const auto& value : domain)
-            cout << "  " << value << endl;
+    int getLineNumber() const {
+        return lineNumber;
     }
 
 private:
-    template<typename T>
-    void printList(const string& label, const vector<T*>& list) const {
-        cout << label << "(" << list.size() << "):" << endl;
-        for (const auto& item : list)
-            cout << "  " << item->toString() << endl;
+    std::istream& input;
+    int lineNumber;
+    std::string tokenValue;
+
+    TokenType readString() {
+        tokenValue.clear();
+        char ch;
+        while (input.get(ch)) {
+            if (ch == '\'') {
+                return TokenType::STRING;
+            } else {
+                tokenValue += ch;
+            }
+        }
+        throw std::runtime_error("Unterminated string starting on line " + std::to_string(lineNumber));
+    }
+
+    TokenType readIdentifier() {
+        tokenValue.clear();
+        char ch;
+        while (input.get(ch)) {
+            if (isalnum(ch) || ch == '_') {
+                tokenValue += ch;
+            } else {
+                input.putback(ch); // Put back the non-alphanumeric character
+                return TokenType::ID;
+            }
+        }
+        return TokenType::EOF_TYPE;
     }
 };
 
-// Forward declaration of parsing functions
-DatalogProgram parseDatalogProgram(istream& input);
-Predicate* parseScheme(istream& input);
-Predicate* parseFact(istream& input);
-Rule* parseRule(istream& input);
-Predicate* parseQuery(istream& input);
-Predicate* parseHeadPredicate(istream& input);
-Predicate* parsePredicate(istream& input);
-Parameter* parseParameter(istream& input);
+// Recursive descent parser for Datalog programs
+class Parser {
+public:
+    Parser(Scanner& scanner) : scanner(scanner) {}
 
-// Main parsing function to parse the entire Datalog program
-DatalogProgram parseDatalogProgram(istream& input) {
-    DatalogProgram program;
-
-    string section;
-    while (input >> section) {
-        if (section == "Schemes:")
-            while (true) {
-                Predicate* scheme = parseScheme(input);
-                if (!scheme)
-                    break;
-                program.schemes.push_back(scheme);
-            }
-        else if (section == "Facts:")
-            while (true) {
-                Predicate* fact = parseFact(input);
-                if (!fact)
-                    break;
-                program.facts.push_back(fact);
-                // Add strings from facts to domain
-                for (auto& param : fact->parameters)
-                    if (param->isString)
-                        program.addToDomain(param->value);
-            }
-        else if (section == "Rules:")
-            while (true) {
-                Rule* rule = parseRule(input);
-                if (!rule)
-                    break;
-                program.rules.push_back(rule);
-            }
-        else if (section == "Queries:")
-            while (true) {
-                Predicate* query = parseQuery(input);
-                if (!query)
-                    break;
-                program.queries.push_back(query);
-            }
+    DatalogProgram parse() {
+        DatalogProgram program;
+        parseDatalogProgram(program);
+        return program;
     }
 
-    return program;
-}
+private:
+    Scanner& scanner;
 
-// Parse a scheme
-Predicate* parseScheme(istream& input) {
-    string name;
-    if (!(input >> name) || name == "Facts:")
-        return nullptr;
-
-    Predicate* scheme = new Predicate(name);
-
-    string token;
-    if (!(input >> token) || token != "(")
-        throw runtime_error("Expected '(' after scheme name");
-
-    while (true) {
-        Parameter* param = parseParameter(input);
-        if (!param)
-            break;
-        scheme->parameters.push_back(param);
+    void parseDatalogProgram(DatalogProgram& program) {
+        expect(TokenType::SCHEMES);
+        expect(TokenType::COLON);
+        parseSchemeList(program);
+        expect(TokenType::FACTS);
+        expect(TokenType::COLON);
+        parseFactList(program);
+        expect(TokenType::RULES);
+        expect(TokenType::COLON);
+        parseRuleList(program);
+        expect(TokenType::QUERIES);
+        expect(TokenType::COLON);
+        parseQueryList(program);
+        expect(TokenType::EOF_TYPE);
     }
 
-    if (!(input >> token) || token != ")")
-        throw runtime_error("Expected ')' after scheme parameters");
-
-    return scheme;
-}
-
-// Parse a fact
-Predicate* parseFact(istream& input) {
-    string name;
-    if (!(input >> name) || name == "Rules:")
-        return nullptr;
-
-    Predicate* fact = new Predicate(name);
-
-    string token;
-    if (!(input >> token) || token != "(")
-        throw runtime_error("Expected '(' after fact name");
-
-    while (true) {
-        string value;
-        if (!(input >> value))
-            throw runtime_error("Expected value in fact");
-        bool isString = false;
-        if (value.front() == '\'') {
-            if (value.back() != '\'')
-                throw runtime_error("Unclosed string in fact");
-            value = value.substr(1, value.size() - 2);
-            isString = true;
+    void parseSchemeList(DatalogProgram& program) {
+        while (scanner.getNextToken() == TokenType::ID) {
+            program.schemes.push_back(parseScheme());
         }
-        fact->parameters.push_back(new Parameter(value, isString));
-
-        if (!(input >> token))
-            throw runtime_error("Expected ',' or '.' in fact");
-        if (token == ",")
-            continue;
-        else if (token == ")")
-            break;
-        else
-            throw runtime_error("Expected ',' or ')' in fact");
     }
 
-    if (!(input >> token) || token != ".")
-        throw runtime_error("Expected '.' after fact");
-
-    return fact;
-}
-
-// Parse a rule
-Rule* parseRule(istream& input) {
-    Predicate* headPred = parseHeadPredicate(input);
-
-    string token;
-    if (!(input >> token) || token != ":-")
-        throw runtime_error("Expected ':-' after head predicate in rule");
-
-    Rule* rule = new Rule(headPred);
-
-    while (true) {
-        Predicate* bodyPred = parsePredicate(input);
-        if (!bodyPred)
-            break;
-        rule->bodyPredicates.push_back(bodyPred);
+    Predicate parseScheme() {
+        Predicate scheme = parsePredicate();
+        expect(TokenType::PERIOD);
+        return scheme;
     }
 
-    if (!(input >> token) || token != ".")
-        throw runtime_error("Expected '.' after rule body");
-
-    return rule;
-}
-
-// Parse a query
-Predicate* parseQuery(istream& input) {
-    Predicate* query = parsePredicate(input);
-
-    string token;
-    if (!(input >> token) || token != "?")
-        throw runtime_error("Expected '?' at the end of query");
-
-    return query;
-}
-
-// Parse a head predicate
-Predicate* parseHeadPredicate(istream& input) {
-    string name;
-    if (!(input >> name))
-        throw runtime_error("Expected predicate name");
-
-    Predicate* headPred = new Predicate(name);
-
-    string token;
-    if (!(input >> token) || token != "(")
-        throw runtime_error("Expected '(' after head predicate name");
-
-    while (true) {
-        Parameter* param = parseParameter(input);
-        if (!param)
-            break;
-        headPred->parameters.push_back(param);
+    void parseFactList(DatalogProgram& program) {
+        while (scanner.getNextToken() == TokenType::ID) {
+            program.facts.push_back(parseFact());
+        }
     }
 
-    if (!(input >> token) || token != ")")
-        throw runtime_error("Expected ')' after head predicate parameters");
-
-    return headPred;
-}
-
-// Parse a predicate
-Predicate* parsePredicate(istream& input) {
-    string name;
-    if (!(input >> name))
-        return nullptr;
-
-    Predicate* pred = new Predicate(name);
-
-    string token;
-    if (!(input >> token) || token != "(")
-        throw runtime_error("Expected '(' after predicate name");
-
-    while (true) {
-        Parameter* param = parseParameter(input);
-        if (!param)
-            break;
-        pred->parameters.push_back(param);
+    Predicate parseFact() {
+        Predicate fact = parsePredicate();
+        expect(TokenType::PERIOD);
+        return fact;
     }
 
-    if (!(input >> token) || token != ")")
-        throw runtime_error("Expected ')' after predicate parameters");
+    void parseRuleList(DatalogProgram& program) {
+        while (scanner.getNextToken() == TokenType::ID) {
+            program.rules.push_back(parseRule());
+        }
+    }
 
-    return pred;
-}
+    Rule parseRule() {
+        Predicate headPredicate = parsePredicate();
+        expect(TokenType::COLON);
+        expect(TokenType::DASH);
+        std::vector<Predicate> bodyPredicates;
+        do {
+            bodyPredicates.push_back(parsePredicate());
+        } while (scanner.getNextToken() == TokenType::COMMA);
+        expect(TokenType::PERIOD);
+        return Rule(headPredicate, bodyPredicates);
+    }
 
-// Parse a parameter
-Parameter* parseParameter(istream& input) {
-    string token;
-    if (!(input >> token))
-        return nullptr;
+    void parseQueryList(DatalogProgram& program) {
+        while (scanner.getNextToken() == TokenType::ID) {
+            program.queries.push_back(parsePredicate());
+            expect(TokenType::Q_MARK);
+        }
+    }
 
-    if (token == ")")
-        return nullptr;
-    else if (token.front() == '\'') {
-        if (token.back() != '\'')
-            throw runtime_error("Unclosed string in parameter");
-        return new Parameter(token.substr(1, token.size() - 2), true);
-    } else
-        return new Parameter(token, false);
-}
+    Predicate parsePredicate() {
+        std::string name = scanner.getTokenValue();
+        expect(TokenType::ID);
+        expect(TokenType::LEFT_PAREN);
+        std::vector<Parameter> parameters;
+        if (scanner.getNextToken() == TokenType::ID || scanner.getNextToken() == TokenType::STRING) {
+            parameters.push_back(parseParameter());
+            while (scanner.getNextToken() == TokenType::COMMA) {
+                parameters.push_back(parseParameter());
+            }
+        }
+        expect(TokenType::RIGHT_PAREN);
+        return Predicate(name, parameters);
+    }
+
+    Parameter parseParameter() {
+        if (scanner.getNextToken() == TokenType::ID) {
+            return Parameter(scanner.getTokenValue(), false);
+        } else if (scanner.getNextToken() == TokenType::STRING) {
+            return Parameter(scanner.getTokenValue(), true);
+        } else {
+            throw std::runtime_error("Expected parameter on line " + std::to_string(scanner.getLineNumber()));
+        }
+    }
+
+    void expect(TokenType expected) {
+        TokenType actual = scanner.getNextToken();
+        if (actual != expected) {
+            throw std::runtime_error("Expected token type " + tokenTypeToString(expected) + " but got " + tokenTypeToString(actual) + " on line " + std::to_string(scanner.getLineNumber()));
+        }
+    }
+
+    std::string tokenTypeToString(TokenType type) {
+        switch (type) {
+            case TokenType::SCHEMES: return "SCHEMES";
+            case TokenType::FACTS: return "FACTS";
+            case TokenType::RULES: return "RULES";
+            case TokenType::QUERIES: return "QUERIES";
+            case TokenType::COLON: return "COLON";
+            case TokenType::COMMA: return "COMMA";
+            case TokenType::PERIOD: return "PERIOD";
+            case TokenType::Q_MARK: return "Q_MARK";
+            case TokenType::LEFT_PAREN: return "LEFT_PAREN";
+            case TokenType::RIGHT_PAREN: return "RIGHT_PAREN";
+            case TokenType::ID: return "ID";
+            case TokenType::STRING: return "STRING";
+            case TokenType::EOF_TYPE: return "EOF_TYPE";
+        }
+        return "";
+    }
+};
 
 int main() {
-    try {
-        DatalogProgram program = parseDatalogProgram(cin);
-        program.print();
-    } catch (const exception& e) {
-        cerr << "Failure!" << endl;
-        cerr << "  (" << e.what() << ")" << endl;
-    }
+    // Example usage
+    // Create a scanner from input stream
+    Scanner scanner(std::cin);
+    // Create a parser with the scanner
+    Parser parser(scanner);
+    // Parse the Datalog program
+    DatalogProgram program = parser.parse();
+    // Output the parsed program
+    std::cout << "Success!\n" << program.toString() << std::endl;
     return 0;
 }
