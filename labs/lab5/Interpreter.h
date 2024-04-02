9c539ccdab65c99bdc71bf18f451e971caf7dec1
sammothxc <sdwarrior8@gmail.com>
2024-04-02 17:09:37 -0600

    update

--- labs/lab5/Interpreter.h
+++ labs/lab5/Interpreter.h
@@ -1,4 +1,5 @@
 // Interpreter.h
+
 #ifndef INTERPRETER_H
 #define INTERPRETER_H
 
@@ -11,16 +12,25 @@ class Interpreter {
 public:
     static Graph makeGraph(const std::vector<Rule>& rules) {
         Graph graph(rules.size());
+
+        // Loop over rules
         for (size_t i = 0; i < rules.size(); ++i) {
             const Rule& fromRule = rules[i];
             std::cout << "from rule R" << i << ": " << fromRule.toString() << std::endl;
+
+            // Loop over predicates in the body
             for (const Predicate& bodyPredicate : fromRule.getBodyPredicates()) {
                 std::cout << "from body predicate: " << bodyPredicate.toString() << std::endl;
-                for (size_t j = 0; j < rules.size(); ++j) {
-                    const Rule& toRule = rules[j];
-                    std::cout << "to rule R" << j << ": " << toRule.toString() << std::endl;
-                    if (fromRule.getHeadPredicate().getName() == bodyPredicate.getName() &&
-                        toRule.getHeadPredicate().getName() == bodyPredicate.getName()) {
+            }
+
+            // Loop over rules again to find dependencies
+            for (size_t j = 0; j < rules.size(); ++j) {
+                const Rule& toRule = rules[j];
+
+                // Avoid self-dependencies
+                if (i != j) {
+                    // Find matching names and add edge
+                    if (fromRule.getHeadPredicate().getName() == toRule.getHeadPredicate().getName()) {
                         graph.addEdge(i, j);
                         std::cout << "dependency found: (R" << i << ",R" << j << ")" << std::endl;
                     }
--- labs/lab5/main.cpp
+++ labs/lab5/main.cpp
@@ -1,14 +1,29 @@
 // main.cpp
 
 #include <iostream>
-#include "Graph.h"
+#include "Interpreter.h"
 
 int main() {
-    Graph graph(3);
-    graph.addEdge(1, 2);
-    graph.addEdge(1, 0);
-    graph.addEdge(0, 1);
-    graph.addEdge(1, 1);
+    // Predicate names for fake rules
+    // First is name for head predicate
+    // Second is names for body predicates
+    std::pair<std::string, std::vector<std::string>> ruleNames[] = {
+        { "A", { "B" } },
+        { "B", { "B", "A" } },
+    };
+
+    std::vector<Rule> rules;
+
+    for (auto& rulePair : ruleNames) {
+        std::string headName = rulePair.first;
+        Rule rule = Rule(Predicate(headName));
+        std::vector<std::string> bodyNames = rulePair.second;
+        for (auto& bodyName : bodyNames)
+            rule.addBodyPredicate(Predicate(bodyName));
+        rules.push_back(rule);
+    }
+
+    Graph graph = Interpreter::makeGraph(rules);
     std::cout << graph.toString();
 
     return 0;
