ad6a1ddcb14a7036ff0c4d1e5a3c320c1314f07a
sammothxc <sdwarrior8@gmail.com>
2024-04-03 00:54:05 -0600

    update

--- labs/lab5/Interpreter.h
+++ labs/lab5/Interpreter.h
@@ -1,7 +1,6 @@
 #include "Graph.h"
 #include "Rule.h"
 #include <vector>
-#include <iostream>
 #include <unordered_map>
 
 class Interpreter {
@@ -9,25 +8,27 @@ public:
     static Graph makeGraph(const std::vector<Rule>& rules) {
         Graph graph(rules.size());
         std::unordered_map<std::string, int> ruleIndices;
+        int nodeIdCounter = 0;
 
-        // Associate rule names with indices
-        for (size_t i = 0; i < rules.size(); ++i) {
-            const std::string& ruleName = rules[i].getHeadPredicate().getName();
-            ruleIndices[ruleName] = i;
+        // Associate rule names with unique node IDs
+        for (const Rule& rule : rules) {
+            ruleIndices[rule.getHeadPredicate().getName()] = nodeIdCounter++;
         }
 
         // Create edges based on rule dependencies
         for (size_t i = 0; i < rules.size(); ++i) {
             const Rule& fromRule = rules[i];
+            int fromNodeId = ruleIndices[fromRule.getHeadPredicate().getName()];
             for (const Predicate& bodyPredicate : fromRule.getBodyPredicates()) {
                 const std::string& bodyName = bodyPredicate.getName();
                 auto it = ruleIndices.find(bodyName);
                 if (it != ruleIndices.end()) {
                     int toNodeId = it->second;
-                    graph.addEdge(i, toNodeId);
+                    graph.addEdge(fromNodeId, toNodeId);
                 }
             }
         }
+
         return graph;
     }
 };
